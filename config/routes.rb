9c0e20adb8cc2cf82d8075649b1987f00d3a66c1
JulianArnold <julian_arnold@hotmail.com>
2017-05-02 14:56:15 +0100

    probably the last commit this round in the Hartl book; 20170502

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :microposts
   resources :users
 
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
